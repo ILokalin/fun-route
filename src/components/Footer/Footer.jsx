e2d49d1065bc10c15ab8564780ab869b057cbb0c
ILokalin <51790923+ILokalin@users.noreply.github.com>
2019-12-15 19:12:03 +0500

    minor correction for doc's

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function () {
   
   return  (
     <div className="footer">
-      <a className="footer__link" href="https://github.com/ILokalin/fun-rout" rel="noopener noreferrer" target="_blank">Ilya Lokalin. Test of React.JS 2019
+      <a className="footer__link" href="https://github.com/ILokalin/fun-route" rel="noopener noreferrer" target="_blank">Ilya Lokalin. Test of React.JS 2019
       </a>
     </div>
   )
